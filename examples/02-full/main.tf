d2e532b3ef1a0e6853980435ff54149e51172782
Artjoms Laivins <38862673+Artlvns@users.noreply.github.com>
2024-10-24 13:18:39 +0200

    Update main.tf

--- examples/02-full/main.tf
+++ examples/02-full/main.tf
@@ -8,7 +8,7 @@ module "consumption_budget" {
   app_short_name            = "my-budget-full"
   subscription              = "sub-name"
   environment               = "prod"
-  consumption_budgeet_amount = 9000 # in local currency of subscription location
+  consumption_budget_amount = 9000 # in local currency of subscription location
   consumption_budget_notification_cfg = {
     "80_percent_consumed" = {
       contact_emails = ["vaild.dummy@epost.her"]
